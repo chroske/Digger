c73e4cff410c23d46af2d4e4267ef29cdfbb20fb
chroske <chro96ske@gmail.com>
2018-04-28 17:14:19 +0900

    アイテム掘り出し周りの処理を少し変更

--- Assets/Script/GameStageManager.cs
+++ Assets/Script/GameStageManager.cs
@@ -9,6 +9,9 @@ public class GameStageManager : SingletonMonoBehaviourFast<GameStageManager> {
 	BoxCollider2D ItemFieldInDungeon;
 
 	GameObject dungeon;
+	Dictionary<int, ItemController> generatedItemList = new Dictionary<int, ItemController>();
+
+	int itemIdCounter;
 
 	void Start () {
 		Initialize ();
@@ -16,19 +19,24 @@ public class GameStageManager : SingletonMonoBehaviourFast<GameStageManager> {
 
 	public void Initialize(){
         dungeon = GameObject.Find("Dungeons");
-
+		itemIdCounter = 0;
 		GenItem (new Vector2 (0, -5), new Vector2 (1, 1));
+		GenItem (new Vector2 (4, 10), new Vector2 (2, 2));
 	}
 
 	void GenItem(Vector2 popItemPosition, Vector2 popItemSizeScale){
-		var popItemField = Instantiate (ItemFieldInDungeon.gameObject, popItemPosition, Quaternion.identity);
-		popItemField.transform.SetParent (dungeon.transform);
-		popItemField.transform.localScale = popItemSizeScale;
+		//var popItemField = Instantiate (ItemFieldInDungeon.gameObject, popItemPosition, Quaternion.identity);
+		//popItemField.transform.SetParent (dungeon.transform);
+		//popItemField.transform.localScale = popItemSizeScale;
 
 		var popItem = Instantiate (itemPrefab, popItemPosition, Quaternion.identity);
 		popItem.transform.localScale = popItemSizeScale;
+		itemIdCounter++;
+		var itemController = popItem.GetComponent<ItemController> ();
+		itemController.itemId = itemIdCounter;
+		generatedItemList.Add (itemIdCounter, itemController);
 
-		popItemField.GetComponent<BoxCollider2D> ().size = new Vector2 (popItem.GetComponent<BoxCollider2D> ().size.x + 0.5f, popItem.GetComponent<BoxCollider2D> ().size.y + 0.5f);
+		//popItemField.GetComponent<BoxCollider2D> ().size = new Vector2 (popItem.GetComponent<BoxCollider2D> ().size.x + 0.5f, popItem.GetComponent<BoxCollider2D> ().size.y + 0.5f);
 		popItem.GetComponent<Rigidbody2D> ().simulated = true;
 	}
 }
--- Assets/Script/ItemController.cs
+++ Assets/Script/ItemController.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ItemController : MonoBehaviour {
     public int itemId;
+	[SerializeField]
+	BoxCollider2D boxCol2d;
+	[SerializeField]
+	Rigidbody2D rigidbody2D;
 
+	void Start(){
+		rigidbody2D.gravityScale = 0;
+	}
 
     void OnCollisionEnter2D (Collision2D c){
         if(c.gameObject.CompareTag ("my_player_character") || c.gameObject.CompareTag ("other_player_character")){
             Destroy(this.gameObject);
-
-        }
+		}
     }
+
+	void OnTriggerExit2D(Collider2D c){
+		if(c.gameObject.CompareTag ("dungeon")){
+			if(boxCol2d.isTrigger == true){
+				boxCol2d.isTrigger = false;
+				rigidbody2D.gravityScale = 2;
+			}
+		}
+	}
 }
